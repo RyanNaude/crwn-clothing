02327d37434252a5f229ddc2cd45d26eaefdcb3d
ryan-naude <ryan.naude@arwyp.com>
2021-04-29 10:33:27 +0200

    finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.components.jsx
+++ src/components/sign-up/sign-up.components.jsx
@@ -20,9 +20,7 @@ class SignUp extends React.Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
-
     const { displayName, email, password, confirmPassword } = this.state;
-
     if (password !== confirmPassword) {
       alert("Passwords dont match");
       return;
